3ba4d3d8f3eeec79f12903c37ba131f0a5fa83e7
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-19 11:11:52 -0400

    added reset css; put css, js, images in assets folder; api data for events now disp properly on page

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+function buildQueryURL() {
+    let search = $('#search-term').val().trim(); 
+    const APIKEY = 'MTUwOTcwMjd8MTU2MzQ2MzA4Ny42OQ';
+    let initialQueryURL = `https://api.seatgeek.com/2/events?q=${search}&client_id=${APIKEY}`;
+
+    console.log(initialQueryURL);
+    return initialQueryURL;
+}
+
+
+function clear() {
+    $("#event-section").empty();
+}
+
+$("#run-search").on("click", function(event) {
+    event.preventDefault();
+    clear();
+    let queryURL = buildQueryURL();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        // console.log(response.events[0].title);
+        // console.log(response.events[0].datetime_local);
+        // console.log(response.events[0].url);
+        // console.log(response.events[0].venue.display_location);
+        // console.log(response.events[0].venue.name);
+        // console.log(response.events.length);
+        let events = response.events.length; 
+
+        for (let i = 0; i < events; i++) {
+            let title = $(`<li class='event-data'><a href='#'>${response.events[i].title}</a></li>`);
+            let initialDate = response.events[i].datetime_local;
+            let m = moment(initialDate, 'YYYY-MM-DDThh:mm:ss');
+            let convertedDate = $(`<li class='event-data'> ${m.format('ll')} </li>`);
+            let location = $(`<li class='event-data'> ${response.events[i].venue.display_location} </li>`);
+            let venue = $(`<li class='event-data'> ${response.events[i].venue.name} </li>`);
+            let seatgeekURL = $(`<li class='event-data'><a href='${response.events[i].url}' target='_blank'>Tickets</a></li>`);
+
+            let results = $('<ul>').addClass('each-event d-flex flex-row justify-content-around'); 
+            results.append(title, convertedDate, location, venue, seatgeekURL);
+            $('#event-section').append(results);
+    }
+
+    });
+});
+  
+$("#clear-all").on("click", clear);
