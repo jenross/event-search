0b0b3c6334c7c635d3285a9900cc597fe6f143e5
Dean Haider <personal.haider@gmail.com>
2019-07-25 19:45:16 -0400

    fixing merge conflicts

---
+++ dean-navbar/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+              //the 205 is a fixed header offset
+            scrollTop: target.offset().top - 205
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
